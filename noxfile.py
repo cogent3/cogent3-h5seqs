839fb08492c1fe3f33f4f6b46f43f17535db015e
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-04-28 12:55:48 +1000

    DEV: added test coverage option to noxfile

--- noxfile.py
+++ noxfile.py
@@ -13,3 +13,17 @@ def test(session):
         "-x",
         ".",
     )
+
+
+@nox.session(python=[f"3.{v}" for v in _py_versions])
+def test_cov(session):
+    session.install("-e.[dev]")
+    # doctest modules within cogent3/app
+    session.run(
+        "pytest",
+        "--cov-report",
+        "html",
+        "--cov",
+        "cogent3_h5seqs",
+        ".",
+    )
